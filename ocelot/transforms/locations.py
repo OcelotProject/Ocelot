093b87c6abc90cc3416a56338d612d012c980a11
Chris Mutel <cmutel@gmail.com>
2016-05-12 08:02:19 +0200

    Add utility functions for iterating over datasets

--- ocelot/transforms/locations.py
+++ ocelot/transforms/locations.py
@@ -1,11 +1,10 @@
 from . import toolz
-from ..utils import extract_products_as_tuple
+from ..utils import activity_grouper
 
 
 def relabel_global_to_row(data):
-    grouper = lambda x: (x['name'], extract_products_as_tuple(x))
     processed = []
-    for key, datasets in toolz.groupby(grouper, data).items():
+    for key, datasets in toolz.groupby(activity_grouper, data).items():
         if len(datasets) > 1:
             for ds in datasets:
                 if ds['location'] == 'GLO':
--- ocelot/utils.py
+++ ocelot/utils.py
@@ -1,4 +1,14 @@
 def extract_products_as_tuple(dataset):
-    return tuple([exc['name']
+    return tuple(sorted([exc['name']
                   for exc in dataset['exchanges']
-                  if exc['type'] == 'reference product'])
+                  if exc['type'] == 'reference product']))
+
+
+def activity_grouper(dataset):
+    return (dataset['name'], extract_products_as_tuple(dataset))
+
+
+def iterate_exchanges(datasets):
+    for ds in datasets:
+        for exc in ds['exchanges']:
+            yield exc
