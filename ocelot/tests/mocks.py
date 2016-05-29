63f8b82983f9bc590ee22af9a05466774ccd8136
Chris Mutel <cmutel@gmail.com>
2016-05-29 17:56:36 +0200

    Fix mocks for logger/report split

--- ocelot/tests/mocks.py
+++ ocelot/tests/mocks.py
@@ -1,20 +1,25 @@
 from ..model import system_model
-from ..report import Report
+from ..logger import Logger
 from unittest import mock
 import os
 import pytest
 import tempfile
 
 
-class MockReport(Report):
+class MockLogger(Logger):
     """Test mock that uses a tempfile directory, and deletes it after test runs"""
     def create_output_directory(self, report_id):
         self._tempdir = tempfile.TemporaryDirectory()
         return self._tempdir.name
 
-    def finish(self, show=False):
+    def finish(self):
         self.logfile.close()
         self._tempdir.cleanup()
+
+
+def do_nothing(*args, **kwargs):
+    """Mock for `HTMLReport` that doesn't do anything"""
+    pass
 
 
 def passthrough(obj):
@@ -25,12 +30,16 @@ def passthrough(obj):
 @pytest.fixture
 def fake_report(monkeypatch):
     monkeypatch.setattr(
-        'ocelot.model.Report',
-        MockReport
+        'ocelot.model.Logger',
+        MockLogger
     )
     monkeypatch.setattr(
         'ocelot.model.extract_directory',
         passthrough
+    )
+    monkeypatch.setattr(
+        'ocelot.model.HTMLReport',
+        do_nothing
     )
 
 # Test to make sure above fixture correctly monkey-patches stuff
