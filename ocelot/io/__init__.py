10efce0c5a872e036f8fff8f2e41c980b1c6c748
Chris Mutel <cmutel@gmail.com>
2016-08-09 15:08:41 +0200

    Move cache handling to io directory

--- ocelot/io/__init__.py
+++ ocelot/io/__init__.py
@@ -1,4 +1,31 @@
 # -*- coding: utf-8 -*-
-from .extract_ecospold2 import extract_directory
+__all__ = (
+    "extract_directory",
+    "cleanup_data_directory",
+    "dataset_schema",
+    "validate_directory",
+    "validate_directory_against_xsd",
+)
+
+from .extract_ecospold2 import extract_ecospold2_directory
+from ..filesystem import check_cache_directory, get_from_cache, cache_data
+
+
+def extract_directory(data_path, use_cache=True):
+    """Extract ecospold2 files in directory ``dirpath``.
+
+    Uses and writes to cache if ``use_cache`` is ``True``.
+
+    Returns datasets in Ocelot internal format."""
+    if not use_cache:
+        return extract_ecospold2_directory(data_path)
+    elif check_cache_directory(data_path):
+        return get_from_cache(data_path)
+    else:
+        data = extract_directory(data_path)
+        cache_data(data, data_path)
+    return data
+
 from .cleanup import cleanup_data_directory
-from .validate_ecospold2 import validate_directory
+from .validate_ecospold2 import validate_directory_against_xsd, validate_directory
+from .validate_internal import dataset_schema
