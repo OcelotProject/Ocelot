154fe29ea55ee7d3c24125f68d5e9570714e3974
Chris Mutel <cmutel@gmail.com>
2016-08-09 15:10:29 +0200

    Add validate to CLI

--- ocelot/bin/ocelot_cli.py
+++ ocelot/bin/ocelot_cli.py
@@ -2,12 +2,20 @@
 # -*- coding: utf-8 -*-
 """Ocelot command line interface. See https://ocelot.space/ for more info.
 
+Commands:
+
+  * run: Run a system model. Uses the default system model (ecoinvent cutoff) if <config> is not specified. <dirpath> is the input files directory.
+  * cleanup: Delete all model runs more than one week old.
+  * validate: Extract the ecospold2 files in <dirpath> and make sure the extracted data meets the Ocelot internal format. This command doesn't use the extraction cache.
+  * xsd: Validate the ecospold2 files in <dirpath> against the default XSD or another XSD specified in <schema>.
+
 Usage:
   ocelot-cli run <dirpath> [--noshow]
   ocelot-cli run <dirpath> <config> [--noshow]
   ocelot-cli cleanup
-  ocelot-cli validate <dirpath> <schema>
   ocelot-cli validate <dirpath>
+  ocelot-cli xsd <dirpath> <schema>
+  ocelot-cli xsd <dirpath>
   ocelot-cli -l | --list
   ocelot-cli -h | --help
   ocelot-cli --version
@@ -24,6 +32,7 @@ from ocelot import (
     cleanup_data_directory,
     data_dir,
     system_model,
+    validate_directory_against_xsd,
     validate_directory,
 )
 import os
@@ -33,15 +42,17 @@ import sys
 def main():
     try:
         args = docopt(__doc__, version='Ocelot open source linker CLI 0.1')
-        if args['validate']:
-            validate_directory(
+        if args['run']:
+            system_model(args["<dirpath>"], args['<config>'], show=not args['--noshow'])
+        elif args['validate']:
+            validate_directory(args['<dirpath>'])
+        elif args['xsd']:
+            validate_directory_against_xsd(
               args['<dirpath>'],
               args['<schema>'] or os.path.join(data_dir, 'EcoSpold02.xsd')
             )
         elif args['cleanup']:
             cleanup_data_directory()
-        elif args['run']:
-            system_model(args["<dirpath>"], args['<config>'], show=not args['--noshow'])
         else:
             raise ValueError
     except KeyboardInterrupt:
