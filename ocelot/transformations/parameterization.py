4010586681d67298f90ac9417afe241c63f0012a
Chris Mutel <cmutel@gmail.com>
2016-08-10 14:25:28 +0200

    Remove non-working validity check for now

--- ocelot/transformations/parameterization.py
+++ ocelot/transformations/parameterization.py
@@ -15,8 +15,6 @@ def every_exchange_with_formula_has_a_variable_name(data):
 
 def parameter_names_are_unique(data):
     """Data validity check."""
-    # TODO: Broken for now. Not every parameter has a variable name.
-    return data
     for ds in data:
         if not ds.get('parameters'):
             continue
@@ -27,5 +25,5 @@ def parameter_names_are_unique(data):
 
 parameterization_validity_checks = Collection(
     # every_exchange_with_formula_has_a_variable_name,
-    parameter_names_are_unique
+    # parameter_names_are_unique
 )
