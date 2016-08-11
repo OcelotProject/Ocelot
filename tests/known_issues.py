30e0a44497df3a5eb3e07660d5026d9712a66074
Chris Mutel <cmutel@gmail.com>
2016-08-11 11:56:35 +0200

    Add tests for other cleanup functions

--- tests/known_issues.py
+++ tests/known_issues.py
@@ -48,3 +48,82 @@ def test_fix_formulas_parameters():
     expected = copy.deepcopy(data)
     expected[0]['parameters'][0]['formula'] = switched
     assert fix_formulas(data) == expected
+
+def test_fix_clinker_pv():
+    data = [{
+        'name': 'clinker production',
+        'location': 'nowhere',
+        'exchanges': [{'production volume': {
+            'formula': '1 * clinker_PV / everything',
+            'variable': 'clinker_PV'
+    }}]}]
+    expected = [{
+        'name': 'clinker production',
+        'location': 'nowhere',
+        'exchanges': [{'production volume': {
+            'formula': '1 * clinker_pv / everything',
+            'variable': 'clinker_pv'
+    }}]}]
+    assert fix_clinker_pv_variable_name(data) == expected
+
+def test_fix_cement():
+    data = [{
+        'name': 'cement production, alternative constituents 6-20%',
+        'location': 'nowhere',
+        'exchanges': [{
+            'formula': 'frogs + ggbfs',
+            'variable': 'ggbfs',
+        }]
+    }]
+    expected = [{
+        'name': 'cement production, alternative constituents 6-20%',
+        'location': 'nowhere',
+        'exchanges': [{
+            'formula': 'frogs + GGBFS',
+            'variable': 'GGBFS'
+        }]
+    }]
+    assert fix_cement_production_variable_name(data) == expected
+
+def test_fix_ethylene():
+    data = [{
+        'name': 'ethylene glycol production',
+        'location': 'nowhere',
+        'exchanges': [{'formula': 'yield fair knight'}],
+        'parameters': [{'variable': 'yield'}]
+    }]
+    expected = [{
+        'name': 'ethylene glycol production',
+        'location': 'nowhere',
+        'exchanges': [{'formula': 'Yield fair knight'}],
+        'parameters': [{'variable': 'Yield'}]
+    }]
+    assert fix_ethylene_glycol_uses_yield(data) == expected
+
+def test_petroleum():
+    data = [{
+        'name': 'petroleum and gas production, off-shore',
+        'location': 'nowhere',
+        'exchanges': [{'formula': 'HPV vaccination != petroleum_APV'}]
+    }]
+    expected = [{
+        'name': 'petroleum and gas production, off-shore',
+        'location': 'nowhere',
+        'exchanges': [{'formula': 'HPV vaccination != petroleum_apv'}]
+    }]
+    assert fix_offshore_petroleum_variable_name(data) == expected
+
+def test_fix_benzene():
+    data = [{
+        'name': 'benzene chlorination',
+        'location': 'nowhere',
+        'exchanges': [{'production volume': {
+            'formula': '1 * UnitConversion(152000000, \'pound avoirdupois\', \'kg\')',
+    }}]}]
+    expected = [{
+        'name': 'benzene chlorination',
+        'location': 'nowhere',
+        'exchanges': [{'production volume': {
+            'formula': '1 * 68949040.24',
+    }}]}]
+    assert fix_benzene_chlorination_unit(data) == expected
