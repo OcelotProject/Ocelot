d5ff124c924b2ebfe5e683e01fa20a3666fe15aa
Chris Mutel <cmutel@gmail.com>
2016-08-12 15:27:41 +0200

    Move some transformation utils

---
+++ ocelot/transformations/utils.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+
+def extract_products_as_tuple(dataset):
+    return tuple(sorted([exc['name']
+                  for exc in dataset['exchanges']
+                  if exc['type'] == 'reference product']))
+
+
+def activity_grouper(dataset):
+    return (dataset['name'], extract_products_as_tuple(dataset))
+
+
+def iterate_exchanges(datasets):
+    for ds in datasets:
+        for exc in ds['exchanges']:
+            yield exc
--- ocelot/utils.py
+++ ocelot/utils.py
@@ -1,22 +1,5 @@
 # -*- coding: utf-8 -*-
-import os
 import functools
-
-
-def extract_products_as_tuple(dataset):
-    return tuple(sorted([exc['name']
-                  for exc in dataset['exchanges']
-                  if exc['type'] == 'reference product']))
-
-
-def activity_grouper(dataset):
-    return (dataset['name'], extract_products_as_tuple(dataset))
-
-
-def iterate_exchanges(datasets):
-    for ds in datasets:
-        for exc in ds['exchanges']:
-            yield exc
 
 
 def get_function_meta(function):
