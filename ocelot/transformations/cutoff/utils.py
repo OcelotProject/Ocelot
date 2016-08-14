b219211284ca6bd9e1c8e35cbaaa7197faf32f01
Chris Mutel <cmutel@gmail.com>
2016-08-14 09:53:02 +0200

    Add function to make ecoinvent happy

--- ocelot/transformations/cutoff/utils.py
+++ ocelot/transformations/cutoff/utils.py
@@ -11,3 +11,20 @@ def apply_allocation_factors(dataset, factors):
 def needs_allocation(wrapped, instance, args, kwargs):
     dataset, factors = wrapped(*args, **kwargs)
     return apply_allocation_factors(dataset, factors)
+
+
+def flip_non_allocatable_byproducts(dataset):
+    """Change non-allocatable byproducts from outputs to technosphere to inputs from technosphere.
+
+    Non-allocatable byproducts have the classification ``recyclable`` or ``waste``.
+
+    This has no effect on the technosphere matrix, and should not change the behaviour of any transformation functions, which should be testing for classification instead of exchange type. However, this is the current behaviour of the existing ecoinvent system model.
+
+    Change something from an output to an input requires flipping the sign of any numeric fields."""
+    for exc in dataset['exchanges']:
+        if exc['type'] == 'byproduct' and exc['byproduct classification'] != 'allocatable':
+            exc['type'] = 'from technosphere'
+            exc['amount'] = -1 * exc['amount']
+            if 'formula' in exc:
+                exc['formula'] = '-1 * ({})'.format(exc['formula'])
+    return dataset
