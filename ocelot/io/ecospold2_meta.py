e2186d6880006e9b475e34c8daa56bebc6cc5f9d
Chris Mutel <cmutel@gmail.com>
2016-08-18 10:57:47 +0200

    Use named groups in 'Ref' re

--- ocelot/io/ecospold2_meta.py
+++ ocelot/io/ecospold2_meta.py
@@ -77,9 +77,9 @@ UUID = '[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}'
 
 REFERENCE_REGULAR_EXPRESSIONS = {
     # Exchange amount and parameters
-    'exchange': re.compile("Ref\('{uuid}'\)".format(uuid=UUID)),
+    'exchange': re.compile("Ref\('(?P<uuid>{uuid})'\)".format(uuid=UUID)),
     # Exchange production volume
-    'pv': re.compile("Ref\('{uuid}', 'ProductionVolume'\)".format(uuid=UUID)),
+    'pv': re.compile("Ref\('(?P<uuid>{uuid})', 'ProductionVolume'\)".format(uuid=UUID)),
     # Exchange property
-    'property': re.compile("Ref\('{uuid}', '{uuid}'\)".format(uuid=UUID)),
+    'property': re.compile("Ref\('(?P<first>{uuid})', '(?P<second>{uuid})'\)".format(uuid=UUID)),
 }
--- tests/io/ecospold2_meta.py
+++ tests/io/ecospold2_meta.py
@@ -13,3 +13,14 @@ def test_references():
     assert REFERENCE_REGULAR_EXPRESSIONS['exchange'].findall(test_string)
     assert REFERENCE_REGULAR_EXPRESSIONS['pv'].findall(test_string)
     assert REFERENCE_REGULAR_EXPRESSIONS['property'].findall(test_string)
+
+    get_list = lambda x: [match.groupdict() for match in x.finditer(test_string)]
+
+    assert get_list(REFERENCE_REGULAR_EXPRESSIONS['exchange']) == [{'uuid': 'b48dfb3c-d9c6-4023-a424-eabcf6141198'}]
+    assert get_list(REFERENCE_REGULAR_EXPRESSIONS['pv']) == [{'uuid': 'a0d4e28e-d844-430e-8b6c-e62ddbce192f'}]
+    assert get_list(REFERENCE_REGULAR_EXPRESSIONS['property']) == [{
+        'first': 'a0d4e28e-d844-430e-8b6c-e62ddbce192f',
+        'second': 'b48dfb3c-d9c6-4023-a424-eabcf6141198'
+    }]
+
+
