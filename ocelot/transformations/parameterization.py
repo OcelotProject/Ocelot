dc9e1568aecdadaa7877e4975e249849c4324065
Chris Mutel <cmutel@gmail.com>
2016-08-19 15:17:54 +0200

    Subclass ParameterSet to handle division by zero

--- ocelot/transformations/parameterization.py
+++ ocelot/transformations/parameterization.py
@@ -157,17 +157,44 @@ def extract_named_parameters(dataset):
             if 'variable' in exc}
 
 
+class TolerantParameterSet(ParameterSet):
+    """Subclass of ParameterSet that sets flawed formulas to zero"""
+    def evaluate(self):
+        interpreter = Interpreter()
+        result = {}
+        for key in self.order:
+            # No global params in Ocelot
+            # if key in self.global_params:
+            #     interpreter.symtable[key] = self.global_params[key]
+            if self.params[key].get('formula'):
+                try:
+                    value = interpreter(self.params[key]['formula'])
+                except ZeroDivisionError:
+                    # Grumble grumble...
+                    value = 0
+                interpreter.symtable[key] = result[key] = value
+            elif 'amount' in self.params[key]:
+                interpreter.symtable[key] = result[key] = self.params[key]['amount']
+            else:
+                raise ValueError("No suitable formula or static amount found "
+                                 "in {}".format(key))
+        return result
+
+
 def recalculate(dataset):
     # Set up an Interpreter with each variable name and its value
     interpreter = Interpreter()
-    parameter_set = ParameterSet(extract_named_parameters(dataset))
+    parameter_set = TolerantParameterSet(extract_named_parameters(dataset))
     for key, value in parameter_set.evaluate().items():
         interpreter.symtable[key] = value
 
     # Update each parameterized exchange
     for exc in iterate_all_parameters(dataset):
         if 'formula' in exc:
-            exc['amount'] = interpreter(exc['formula'])
+            try:
+                exc['amount'] = interpreter(exc['formula'])
+            except ZeroDivisionError:
+                exc['amount'] = 0
         elif 'variable' in exc:
             exc['amount'] = interpreter.symtable[exc['variable']]
         else:
