a6abfbcd99bd64734c271350e2bec71c3f6dc680
Chris Mutel <cmutel@gmail.com>
2016-08-20 20:43:01 +0200

    Validity tests for market datasets. Closes #14.

--- ocelot/errors.py
+++ ocelot/errors.py
@@ -11,3 +11,13 @@ class ParameterizationError(Exception):
 
 class MultipleGlobalDatasets(Exception):
     pass
+
+
+class InvalidMarketExchange(Exception):
+    """Markets aren't allowed to consume their own reference product"""
+    pass
+
+
+class InvalidMarket(Exception):
+    """Markets can only have one reference product"""
+    pass
