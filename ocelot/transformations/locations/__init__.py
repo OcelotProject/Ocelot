f780b40c8036ac2f6700ac59830333ee738d8cb5
Chris Mutel <cmutel@gmail.com>
2016-08-30 20:58:51 +0200

    Instantiate topology on import

--- ocelot/transformations/locations/__init__.py
+++ ocelot/transformations/locations/__init__.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
-from .topology import Topology
+from ._topology import Topology
 from .rest_of_world import relabel_global_to_row
 from .validation import check_single_global_dataset
+
+topology = Topology()
