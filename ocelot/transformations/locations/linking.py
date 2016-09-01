dfe531f24a3c64d74225798e7c85fcc4cac89c72
Chris Mutel <cmutel@gmail.com>
2016-09-01 22:10:10 +0200

    Add actualize activity links function

--- ocelot/transformations/locations/linking.py
+++ ocelot/transformations/locations/linking.py
@@ -1,8 +1,22 @@
 # -*- coding: utf-8 -*-
+from ... import toolz
+from ...errors import UnresolvableActivityLink
+from ..utils import get_single_reference_product
 
 
 def actualize_activity_links(data):
     """Add ``code`` field to activity links."""
-    pass
-    # Raise an error if not found
-    # Raise an error if multiple possible
+    mapping = toolz.groupby("id", data)
+    link_iterator = (exc
+                     for ds in data
+                     for exc in ds['exchanges']
+                     if exc.get("activity link"))
+    for link in link_iterator:
+        references = [ds
+                      for ds in mapping[link['activity link']]
+                      if ds['reference product'] == link['name']]
+        if len(references) != 1:
+            message = "Can't resolve activity link: {}"
+            raise UnresolvableActivityLink(message.format(link['activity link']))
+        link['code'] = references[0]['code']
+    return data
---
+++ tests/locations/linking.py
@@ -0,0 +1,78 @@
+from ocelot.errors import UnresolvableActivityLink
+from ocelot.transformations.locations.linking import actualize_activity_links
+import pytest
+
+
+def test_actualize_activity_links():
+    given = [{
+        'code': 'find me',
+        'id': 'the right one',
+        'exchanges': [],
+        'reference product': 'foo',
+    }, {
+        'code': 'oops',
+        'id': 'the right one',
+        'exchanges': [],
+        'reference product': 'bar',
+    }, {
+        'id': '',
+        'exchanges': [{
+            'activity link': 'the right one',
+            'name': 'foo',
+        }]
+    }]
+    expected = [{
+        'code': 'find me',
+        'id': 'the right one',
+        'exchanges': [],
+        'reference product': 'foo',
+    }, {
+        'code': 'oops',
+        'id': 'the right one',
+        'exchanges': [],
+        'reference product': 'bar',
+    }, {
+        'id': '',
+        'exchanges': [{
+            'activity link': 'the right one',
+            'code': 'find me',
+            'name': 'foo',
+        }]
+    }]
+    assert actualize_activity_links(given) == expected
+
+def test_actualize_activity_links_errors():
+    too_many = [{
+        'code': 'find me',
+        'id': 'the right one',
+        'exchanges': [],
+        'reference product': 'foo',
+    }, {
+        'code': 'oops',
+        'id': 'the right one',
+        'exchanges': [],
+        'reference product': 'foo',
+    }, {
+        'id': '',
+        'exchanges': [{
+            'activity link': 'the right one',
+            'name': 'foo',
+        }]
+    }]
+    with pytest.raises(UnresolvableActivityLink):
+        actualize_activity_links(too_many)
+
+    too_few = [{
+        'code': 'find me',
+        'id': 'the right one',
+        'exchanges': [],
+        'reference product': 'bar',
+    }, {
+        'id': '',
+        'exchanges': [{
+            'activity link': 'the right one',
+            'name': 'foo',
+        }]
+    }]
+    with pytest.raises(UnresolvableActivityLink):
+        actualize_activity_links(too_few)
