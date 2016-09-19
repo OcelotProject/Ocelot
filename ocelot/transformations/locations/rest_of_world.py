a4eabb9c7626fa93021434c137777fa57873ff46
Chris Mutel <cmutel@gmail.com>
2016-09-19 18:17:50 +0200

    Simplify calculation of region-specific PVs

--- ocelot/transformations/locations/rest_of_world.py
+++ ocelot/transformations/locations/rest_of_world.py
@@ -7,40 +7,32 @@ import logging
 
 
 def relabel_global_to_row(data):
-     """Change ``GLO`` locations to ``RoW`` if there are region-specific datasets in the activity group."""
+    """Change ``GLO`` locations to ``RoW`` if there are region-specific datasets in the activity group."""
     processed = []
-    for key, datasets in toolz.groupby(activity_grouper, data).items(): 
-        
+    for key, datasets in toolz.groupby(activity_grouper, data).items():
         if len(datasets) > 1:
-            # start non-GLO production volume at zero and set ds_GLO (the potential global dataset) to None. 
-            # This is so if there's no GLO it doesnt add a second version of the one from the previous loop
-            non_GLO_pv = 0
-            ds_GLO = None
-            
             check_single_global_dataset(datasets)
-            
+
             for ds in datasets:
-                # Identify and store the GLO dataset, change it to RoW, but don't add to processed yet
                 if ds['location'] == 'GLO':
-                    ds_GLO = ds
+                    # Need to adjust production volume by subtracting the region-specific
+                    # production volumes. We assume that each dataset has a single reference
+                    # product with a production volume.
+                    region_specific_pv = sum(
+                        get_single_reference_product(obj)['production volume']['amount']
+                        for obj in datasets
+                        if obj != ds
+                    )
+                    rp = get_single_reference_product(ds)
+                    rp['production volume']['amount'] -= region_specific_pv
+
                     ds['location'] = 'RoW'
                     logging.info({
                         'type': 'table element',
+                        # key is activity name and list of reference products
                         'data': (key[0], "; ".join(sorted(key[1])))
                     })
-                # get the production volume from the non-GLO transformations and add it to the running total, then add it to processed
-                else:
-                    rp = get_single_reference_product(ds)
-                    non_GLO_pv += rp['production volume']['amount']
-
-                    processed.append(ds)
-
-            # subtract the non-GLO production volume from the GLO/RoW process and then append it too
-            if ds_GLO is not None:
-                rp_GLO = get_single_reference_product(ds_GLO)
-                rp_GLO['production volume']['amount'] -= non_GLO_pv
-                processed.append(ds_GLO)
-
+                processed.append(ds)
         else:
             processed.extend(datasets)
     return processed
