d52ee1a9e49ddc16fb9d5a96d4f64356eb68a484
Chris Mutel <cmutel@gmail.com>
2016-11-16 14:17:18 +0100

    Add topology tree function

--- tests/locations/topology.py
+++ tests/locations/topology.py
@@ -15,6 +15,9 @@ def test_topology_contained():
     with pytest.raises(KeyError):
         topology.contained('foo')
 
+def test_topology_contained_exclude_self():
+    assert topology.contained('RU', exclude_self=True) == {'Russia (Asia)', 'Russia (Europe)'}
+
 def test_topology_intersects():
     assert 'UN-EUROPE' in topology.intersects('DE')
     assert 'DE' in topology.intersects('DE')
@@ -26,7 +29,6 @@ def test_topology_intersects():
     # Test new Ecoinvent 3.3 compatablility labels
     assert topology.intersects('IAI Area, Asia, without China and GCC')
     assert topology.intersects('IAI Area, Gulf Cooperation Council')
-
     with pytest.raises(KeyError):
         topology.intersects('foo')
 
@@ -48,32 +50,70 @@ def test_topology_contains():
 def test_topology_tree():
     given = [
         # market group for electricity, low voltage, ecoinvent 3.3
+        {'location': 'GLO'},
         {'location': 'CA'},
         {'location': 'Canada without Quebec'},
         {'location': 'RNA'},
+        {'location': 'US'},
         {'location': 'CN'},
         {'location': 'ENTSO-E'},
         {'location': 'Europe without Switzerland'},
-        {'location': 'GLO'},
+        {'location': 'RER'},
+        {'location': 'UCTE'},
         {'location': 'RAF'},
         {'location': 'RAS'},
-        {'location': 'RER'},
         {'location': 'RLA'},
         {'location': 'RME'},
-        {'location': 'UCTE'},
-        {'location': 'US'},
     ]
-    expected = [{
-        'GLO': [{
-            'RNA': [{
-                'CA': [{
-                    'Canada without Quebec': []
-                }]
-            }]
-        }]
-    }]
+    expected = {
+        'GLO': {
+            'RNA': {
+                'CA': {'Canada without Quebec': {}},
+                'US': {}
+            },
+            'RAF': {},
+            'RAS': {
+                "CN": {},
+                "RME": {}
+            },
+            'RLA': {},
+            'RER': {
+                'ENTSO-E': {
+                    'UCTE': {}
+                },
+                'Europe without Switzerland': {}
+            }
+        }
+    }
     assert topology.tree(given) == expected
 
 def test_topology_tree_row():
-    pass
+    given = [
+        {'location': 'CA'},
+        {'location': 'GLO'},
+        {'location': 'RoW'},
+    ]
+    expected = {
+        'GLO': {
+            'CA': {},
+            'RoW': {}
+        }
+    }
+    assert topology.tree(given) == expected
+    given = [
+        {'location': 'CA'},
+        {'location': 'RoW'},
+    ]
+    expected = {
+        'CA': {},
+        'RoW': {}
+    }
+    assert topology.tree(given) == expected
 
+def test_topology_tree_glo():
+    given = [
+        {'location': 'CA'},
+        {'location': 'GLO'},
+    ]
+    expected = {'GLO': {'CA': {}}}
+    assert topology.tree(given) == expected
