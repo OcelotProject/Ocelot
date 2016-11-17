34072220f3f6e18c9786f82c1bcde62764ea28f4
Chris Mutel <cmutel@gmail.com>
2016-11-17 12:17:01 +0100

    Finish populating market group suppliers

--- ocelot/transformations/locations/market_groups.py
+++ ocelot/transformations/locations/market_groups.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 from . import topology
-from .markets import allocate_suppliers, annotate_exchange
-from ..utils import get_single_reference_product
 from ... import toolz
 from ...errors import MarketGroupError
+from ..utils import get_single_reference_product
+from .markets import allocate_suppliers, annotate_exchange
+import copy
 import itertools
+import logging
+
+logger = logging.getLogger('ocelot')
 
 
 def link_market_group_suppliers(data):
@@ -14,23 +18,42 @@ def link_market_group_suppliers(data):
         'reference product',
         filter(filter_func, data)
     ))
-    
+
     # Check to make sure names are consistent
     for group in market_groups.values():
         if not len({ds['name'] for ds in group}) == 1:
             raise MarketGroupError("Inconsistent activity names in market group")
 
     for ref_product, groups in market_groups.items():
-        suppliers = [ds for ds in data 
+        suppliers = [ds for ds in data
                      if ds['type'] == 'market activity'
                      and ds['reference product'] == ref_product]
 
+        # Put groups second so that if there are duplicates, the group will be retrieved
         location_lookup = {x['location']: x for x in suppliers}
+        supplier_lookup = copy.deepcopy(location_lookup)
         location_lookup.update({x['location']: x for x in groups})
-        if not len(location_lookup) == len(suppliers) + len(groups):
-            raise MarketGroupError("Market groups can't have same location as markets")
 
         tree = topology.tree(itertools.chain(suppliers, groups))
+
+        if [1 for x in groups if x['location'] == 'RoW']:
+            # Handling RoW is a little tricky. The RoW market group can contain
+            # markets which are not covered by other market groups. So we have
+            # to resolve what RoW means in each context.
+            row_faces = topology('__all__').difference(
+                set.union(*[topology(x['location']) for x in groups])
+            )
+            # This will include RoW, if present, but not GLO
+            row_activities = [x for x in suppliers
+                              if not topology(x['location']).difference(row_faces)
+                              and x['location'] != 'GLO']
+
+            # RoW suppliers need to be removed from GLO suppliers
+            if 'GLO' in tree:
+                for obj in row_activities:
+                    if (obj['location'] != 'RoW'
+                        and obj['location'] in tree['GLO']):
+                        del tree['GLO'][obj['location']]
 
         # Turn `tree` from nested dictionaries to flat list of key, values.
         # Breadth first search
@@ -48,14 +71,44 @@ def link_market_group_suppliers(data):
             if children and not location_lookup[loc]['type'] == 'market group':
                 raise MarketGroupError
 
+        def translate(obj):
+            return annotate_exchange(get_single_reference_product(obj), obj)
+
         for parent, children in flat[::-1]:
-            if not children:
+            print(parent, children)
+            # Special case RoW
+            if parent == 'RoW':
+                obj = location_lookup[parent]
+                obj['suppliers'] = [translate(act) for act in row_activities]
+            else:
+                obj = location_lookup[parent]
+                obj['suppliers'] = [translate(location_lookup[child])
+                                    for child in children]
+
+            # Also add supplier if market and market group have same location
+            if (parent in supplier_lookup
+                and location_lookup[parent]['type'] == 'market group'
+                and parent != 'RoW'):
+                obj['suppliers'].append(translate(supplier_lookup[parent]))
+
+            # For consistency in testing
+            obj['suppliers'].sort(key=lambda x: x['code'])
+
+            for exc in obj['suppliers']:
+                logger.info({
+                    'type': 'table element',
+                    'data': (obj['name'], obj['location'], exc['location'])
+                })
+
+            if not obj['suppliers']:
+                del obj['suppliers']
                 continue
-            parent = location_lookup[parent]
-            parent['suppliers'] = [annotate_exchange(
-                    get_single_reference_product(location_lookup[obj]),
-                    location_lookup[obj]
-                ) for obj in children]
-            allocate_suppliers(parent)
+
+            allocate_suppliers(obj)
 
     return data
+
+link_market_group_suppliers.__table__ = {
+    'title': "Link and allocate suppliers for market groups. Suppliers can be market activities or other market groups.",
+    'columns': ["Name", "Location", "Supplier Location"]
+}
--- tests/locations/market_groups.py
+++ tests/locations/market_groups.py
@@ -12,6 +12,9 @@ class FakeTopology:
             }
         }
 
+    def __call__(self, x):
+        return set()
+
 
 @pytest.fixture(scope="function")
 def group_fixture(monkeypatch):
@@ -66,7 +69,6 @@ def group_fixture(monkeypatch):
     }]
     return data
 
-
 def test_link_market_group_suppliers(group_fixture):
     expected = [{
         'type': 'market activity',
@@ -94,14 +96,15 @@ def test_link_market_group_suppliers(group_fixture):
         'code': '3',
         'name': 'market group for foo',
         'reference product': 'foo',
-        'suppliers': [{'code': '4',
+        'suppliers': [{'code': '2',
+                       'location': 'M2',
+                       'name': 'market for foo',
+                       'type': 'reference product'},
+                      {'code': '4',
                        'location': 'G2',
                        'name': 'market group for foo',
                        'type': 'reference product'},
-                      {'code': '2',
-                       'location': 'M2',
-                       'name': 'market for foo',
-                       'type': 'reference product'}],
+                      ],
         'exchanges': [{
             'type': 'reference product',
             'name': 'foo',
@@ -122,3 +125,336 @@ def test_link_market_group_suppliers(group_fixture):
         }]
     }]
     assert link_market_group_suppliers(group_fixture) == expected
+
+def test_row_and_glo(monkeypatch):
+    # Test that RoW and GLO are correctly populated
+    # Market groups: GLO, RER (Europe), RoW, Western Europe
+    # Market activities: RoW, CA, FR, NO
+    # GLO Market group: RER (MG), RoW (MG)
+    # RER Market group: Western Europe (MG), NO
+    # Western Europe Market group: FR
+    # RoW Market group: RoW (Market), CA
+    monkeypatch.setattr(
+        'ocelot.transformations.locations.market_groups.allocate_suppliers',
+        lambda x: x
+    )
+    given = [{
+        'type': 'market activity',
+        'location': 'RoW',
+        'code': '1',
+        'name': 'market for foo',
+        'reference product': 'foo',
+        'exchanges': [{'type': 'reference product', 'name': 'foo'}]
+    }, {
+        'type': 'market activity',
+        'location': 'CA',
+        'code': '2',
+        'name': 'market for foo',
+        'reference product': 'foo',
+        'exchanges': [{'type': 'reference product', 'name': 'foo'}]
+    }, {
+        'type': 'market activity',
+        'location': 'FR',
+        'code': '3',
+        'name': 'market for foo',
+        'reference product': 'foo',
+        'exchanges': [{'type': 'reference product', 'name': 'foo'}]
+    }, {
+        'type': 'market activity',
+        'location': 'NO',
+        'code': '4',
+        'name': 'market for foo',
+        'reference product': 'foo',
+        'exchanges': [{'type': 'reference product', 'name': 'foo'}]
+    }, {
+        'type': 'market group',
+        'location': 'GLO',
+        'code': '5',
+        'name': 'market group for foo',
+        'reference product': 'foo',
+        'exchanges': [{'type': 'reference product', 'name': 'foo'}]
+    }, {
+        'type': 'market group',
+        'location': 'RER',
+        'code': '6',
+        'name': 'market group for foo',
+        'reference product': 'foo',
+        'exchanges': [{'type': 'reference product', 'name': 'foo'}]
+    }, {
+        'type': 'market group',
+        'location': 'RoW',
+        'code': '8',
+        'name': 'market group for foo',
+        'reference product': 'foo',
+        'exchanges': [{'type': 'reference product', 'name': 'foo'}]
+    }, {
+        'type': 'market group',
+        'location': 'WEU',
+        'code': '9',
+        'name': 'market group for foo',
+        'reference product': 'foo',
+        'exchanges': [{'type': 'reference product', 'name': 'foo'}]
+    }]
+    expected = [{
+        'type': 'market activity',
+        'location': 'RoW',
+        'code': '1',
+        'name': 'market for foo',
+        'reference product': 'foo',
+        'exchanges': [{'type': 'reference product', 'name': 'foo'}]
+    }, {
+        'type': 'market activity',
+        'location': 'CA',
+        'code': '2',
+        'name': 'market for foo',
+        'reference product': 'foo',
+        'exchanges': [{'type': 'reference product', 'name': 'foo'}]
+    }, {
+        'type': 'market activity',
+        'location': 'FR',
+        'code': '3',
+        'name': 'market for foo',
+        'reference product': 'foo',
+        'exchanges': [{'type': 'reference product', 'name': 'foo'}]
+    }, {
+        'type': 'market activity',
+        'location': 'NO',
+        'code': '4',
+        'name': 'market for foo',
+        'reference product': 'foo',
+        'exchanges': [{'type': 'reference product', 'name': 'foo'}]
+    }, {
+        'type': 'market group',
+        'location': 'GLO',
+        'code': '5',
+        'name': 'market group for foo',
+        'reference product': 'foo',
+        'suppliers': [{'code': '6',
+                       'location': 'RER',
+                       'name': 'market group for foo',
+                       'type': 'reference product'},
+                      {'code': '8',
+                       'location': 'RoW',
+                       'name': 'market group for foo',
+                       'type': 'reference product'}],
+        'exchanges': [{'type': 'reference product', 'name': 'foo'}]
+    }, {
+        'type': 'market group',
+        'location': 'RER',
+        'code': '6',
+        'name': 'market group for foo',
+        'reference product': 'foo',
+        'suppliers': [{'code': '4',
+                       'location': 'NO',
+                       'name': 'market for foo',
+                       'type': 'reference product'},
+                      {'code': '9',
+                       'location': 'WEU',
+                       'name': 'market group for foo',
+                       'type': 'reference product'}],
+        'exchanges': [{'type': 'reference product', 'name': 'foo'}]
+    }, {
+        'type': 'market group',
+        'location': 'RoW',
+        'code': '8',
+        'name': 'market group for foo',
+        'reference product': 'foo',
+        'suppliers': [{'code': '1',
+                       'location': 'RoW',
+                       'name': 'market for foo',
+                       'type': 'reference product'},
+                      {'code': '2',
+                       'location': 'CA',
+                       'name': 'market for foo',
+                       'type': 'reference product'}],
+        'exchanges': [{'type': 'reference product', 'name': 'foo'}]
+    }, {
+        'type': 'market group',
+        'location': 'WEU',
+        'code': '9',
+        'name': 'market group for foo',
+        'reference product': 'foo',
+        'suppliers': [{'code': '3',
+                       'location': 'FR',
+                       'name': 'market for foo',
+                       'type': 'reference product'}],
+        'exchanges': [{'type': 'reference product', 'name': 'foo'}]
+    }]
+    assert link_market_group_suppliers(given) == expected
+
+def test_glo_includes_missing_activities(monkeypatch):
+    monkeypatch.setattr(
+        'ocelot.transformations.locations.market_groups.allocate_suppliers',
+        lambda x: x
+    )
+    given = [{
+        'type': 'market activity',
+        'location': 'CA',
+        'code': '1',
+        'name': 'market for foo',
+        'reference product': 'foo',
+        'exchanges': [{'type': 'reference product', 'name': 'foo'}]
+    }, {
+        'type': 'market activity',
+        'location': 'FR',
+        'code': '3',
+        'name': 'market for foo',
+        'reference product': 'foo',
+        'exchanges': [{'type': 'reference product', 'name': 'foo'}]
+    }, {
+        'type': 'market group',
+        'location': 'GLO',
+        'code': '5',
+        'name': 'market group for foo',
+        'reference product': 'foo',
+        'exchanges': [{'type': 'reference product', 'name': 'foo'}]
+    }, {
+        'type': 'market group',
+        'location': 'RER',
+        'code': '6',
+        'name': 'market group for foo',
+        'reference product': 'foo',
+        'exchanges': [{'type': 'reference product', 'name': 'foo'}]
+    }]
+    expected = [{
+        'type': 'market activity',
+        'location': 'CA',
+        'code': '1',
+        'name': 'market for foo',
+        'reference product': 'foo',
+        'exchanges': [{'type': 'reference product', 'name': 'foo'}]
+    }, {
+        'type': 'market activity',
+        'location': 'FR',
+        'code': '3',
+        'name': 'market for foo',
+        'reference product': 'foo',
+        'exchanges': [{'type': 'reference product', 'name': 'foo'}]
+    }, {
+        'type': 'market group',
+        'location': 'GLO',
+        'code': '5',
+        'name': 'market group for foo',
+        'reference product': 'foo',
+        'suppliers': [{'code': '1',
+                       'location': 'CA',
+                       'name': 'market for foo',
+                       'type': 'reference product'},
+                      {'code': '6',
+                       'location': 'RER',
+                       'name': 'market group for foo',
+                       'type': 'reference product'}],
+        'exchanges': [{'type': 'reference product', 'name': 'foo'}]
+    }, {
+        'type': 'market group',
+        'location': 'RER',
+        'code': '6',
+        'name': 'market group for foo',
+        'reference product': 'foo',
+        'suppliers': [{'code': '3',
+                       'location': 'FR',
+                       'name': 'market for foo',
+                       'type': 'reference product'}],
+        'exchanges': [{'type': 'reference product', 'name': 'foo'}]
+    }]
+    assert link_market_group_suppliers(given) == expected
+
+def test_row_includes_row(monkeypatch):
+    monkeypatch.setattr(
+        'ocelot.transformations.locations.market_groups.allocate_suppliers',
+        lambda x: x
+    )
+    given = [{
+        'type': 'market activity',
+        'location': 'CA',
+        'code': '1',
+        'name': 'market for foo',
+        'reference product': 'foo',
+        'exchanges': [{'type': 'reference product', 'name': 'foo'}]
+    }, {
+        'type': 'market activity',
+        'location': 'RoW',
+        'code': '3',
+        'name': 'market for foo',
+        'reference product': 'foo',
+        'exchanges': [{'type': 'reference product', 'name': 'foo'}]
+    }, {
+        'type': 'market group',
+        'location': 'RoW',
+        'code': '5',
+        'name': 'market group for foo',
+        'reference product': 'foo',
+        'exchanges': [{'type': 'reference product', 'name': 'foo'}]
+    }]
+    expected = [{
+        'type': 'market activity',
+        'location': 'CA',
+        'code': '1',
+        'name': 'market for foo',
+        'reference product': 'foo',
+        'exchanges': [{'type': 'reference product', 'name': 'foo'}]
+    }, {
+        'type': 'market activity',
+        'location': 'RoW',
+        'code': '3',
+        'name': 'market for foo',
+        'reference product': 'foo',
+        'exchanges': [{'type': 'reference product', 'name': 'foo'}]
+    }, {
+        'type': 'market group',
+        'location': 'RoW',
+        'code': '5',
+        'name': 'market group for foo',
+        'reference product': 'foo',
+        'suppliers': [{'code': '1',
+                       'location': 'CA',
+                       'name': 'market for foo',
+                       'type': 'reference product'},
+                      {'code': '3',
+                       'location': 'RoW',
+                       'name': 'market for foo',
+                       'type': 'reference product'}],
+        'exchanges': [{'type': 'reference product', 'name': 'foo'}]
+    }]
+    assert link_market_group_suppliers(given) == expected
+
+def test_same_location_market_group_market(monkeypatch):
+    monkeypatch.setattr(
+        'ocelot.transformations.locations.market_groups.allocate_suppliers',
+        lambda x: x
+    )
+    given = [{
+        'type': 'market activity',
+        'location': 'CA',
+        'code': '1',
+        'name': 'market for foo',
+        'reference product': 'foo',
+        'exchanges': [{'type': 'reference product', 'name': 'foo'}]
+    }, {
+        'type': 'market group',
+        'location': 'CA',
+        'code': '5',
+        'name': 'market group for foo',
+        'reference product': 'foo',
+        'exchanges': [{'type': 'reference product', 'name': 'foo'}]
+    }]
+    expected = [{
+        'type': 'market activity',
+        'location': 'CA',
+        'code': '1',
+        'name': 'market for foo',
+        'reference product': 'foo',
+        'exchanges': [{'type': 'reference product', 'name': 'foo'}]
+    }, {
+        'type': 'market group',
+        'location': 'CA',
+        'code': '5',
+        'name': 'market group for foo',
+        'reference product': 'foo',
+        'suppliers': [{'code': '1',
+                       'location': 'CA',
+                       'name': 'market for foo',
+                       'type': 'reference product'}],
+        'exchanges': [{'type': 'reference product', 'name': 'foo'}]
+    }]
+    assert link_market_group_suppliers(given) == expected
